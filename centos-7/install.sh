da506a30100e9a435f20d6d3db7da820237be489
lightcode <lightcode@gmx.com>
2016-07-02 13:27:13 +0200

    Get thinner Centos 7 image

--- centos-7/install.sh
+++ centos-7/install.sh
@@ -1,15 +1,36 @@
 #!/bin/sh
 
-# Install package from EPEL
+#########################################################
+# Install packages from EPEL
+#########################################################
+
 yum install -y epel-release
 yum install --enablerepo=epel -y htop
 
-# Cleanup log files
-find /var/log -type f | while read f; do echo -ne '' > $f; done;
 
-# remove under tmp directory
-rm -rf /tmp/* /var/tmp
+#########################################################
+# Clean image
+#########################################################
 
 find /etc/sysconfig/network-scripts/ifcfg-* \
-  -type f -maxdepth 0 -not -name ifcfg-lo \
+  -maxdepth 0 -type f -not -name ifcfg-lo \
   -exec sed -i -e '/^UUID/d' -e '/^HWADDR/d' {} \;
+
+# Blank all log files
+find /var/log -type f | while read f; do echo -n '' > $f; done
+
+rm -rf /tmp/* /var/tmp/*
+yum clean all
+
+# Zero out the rest of the free space using dd, then delete the written file.
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+
+# Clear swap
+swap="/dev/centos/swap"
+swapoff "${swap}"
+dd if=/dev/zero of="${swap}" bs=1M
+mkswap "${swap}"
+
+# Add `sync` so Packer doesn't quit too early, before the large file is deleted.
+sync
